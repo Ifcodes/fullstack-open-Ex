abb4fa0dee942ed1a2cb9b03176d3dc05793b0b7
ifcodes <ifeco133@gmail.com>
2020-11-11 07:56:10 +0100

    feat: exercise 1.3,1.4 and 1.5 using object instead of individually assigned variables

--- part1/courseinfo/step1/src/index.js
+++ part1/courseinfo/step1/src/index.js
@@ -1,76 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-  
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
- 
+const Part =  (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
 
-
-const Course = (props) => {
+ const Course = (props) => {
   return (
     <div>
       <h1>{props.course}</h1>
     </div>
   )
 }
-// Start of exercise 1.2
-const Parts = (props) => {
-  return (
-    <div>
-      <p>{props.chapter1} {props.exercise1}</p>
-      <p>{props.chapter2} {props.exercise2}</p>
-      <p>{props.chapter3} {props.exercise3}</p>
-    </div>
-  )
-}
-const Content = (props) => {
+
+const Content = ({parts}) => {
   return (
    <div>
-    <Parts chapter1={part1} exercise1={exercises1} />
-    <Parts chapter2={part2} exercise2={exercises2} />
-    <Parts chapter3={part3} exercise3={exercises3} />
+    <p>{parts.map((part, partIdx)=> <Part key={partIdx} name={part.name} exercises={part.exercises} />)} </p> 
    </div>
   )
 }
-// end of exercise 1.2
-const Total = (props) => {
+
+const Total = ({parts}) => {
   return (
     <div>
-      <p>Number of exercises = {props.amount}</p>
+      <p>Number of exercises = {parts.reduce((acc, part) => acc + part.exercises, 0)}</p>
     </div>
   )
 }
 
 const App = () =>{
   const course = 'Half Stack application development'
-  const total = exercises1 + exercises2 + exercises3 
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    } 
+  ]
+
+  // const total = part1.exercises + part2.exercises + part3.exercises
+
   return (
     <div>
       <Course course={course}/>
-      <Content />
-      <Total amount={total} />
+      <Content parts={parts}/>
+      <Total parts={parts} />
     </div>
   )
-}
+}  
 
 ReactDOM.render(<App />, document.getElementById('root'))
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
